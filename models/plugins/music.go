acac2f69294a12621796f915e3e5e6aa2c302555
chennanrun <naryn_chen@qq.com>
2017-10-19 19:41:11 +0800

    update plugin run thread

--- models/plugins/music.go
+++ models/plugins/music.go
@@ -1,6 +1,14 @@
 package plugins
 
-import "time"
+import (
+	"os"
+	"os/signal"
+	"github.com/bobertlo/go-mpg123/mpg123"
+	"encoding/binary"
+	"bytes"
+	"github.com/gordonklaus/portaudio"
+	"strings"
+)
 
 func init() {
 	p := plugin_music{}
@@ -22,6 +30,68 @@ func (this plugin_music) Active() bool {
 	return true
 }
 
+/**
+  控制函数,返回控制指令。
+ */
+func (this plugin_music) Control(str string) string {
+	if strings.Contains(str, "播放"){
+		return CMD_RESET
+	}
+
+	if strings.Contains(str, "暂停"){
+		return CMD_PAUSE
+	}
+
+	if strings.Contains(str, "停止") || strings.Contains(str, "关闭"){
+		return CMD_STOP
+	}
+
+	return true
+}
+
 func (this plugin_music) Run(msg string) {
-	Say("好的, 现在 " + time.Now().Format("2006-01-02 15:04"))
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, os.Kill)
+
+	// create mpg123 decoder instance
+	decoder, err := mpg123.NewDecoder("")
+	chk(err)
+
+	fileName := "yuanzougaofei.mp3"
+	chk(decoder.Open(fileName))
+	defer decoder.Close()
+
+	// get audio format information
+	rate, channels, _ := decoder.GetFormat()
+
+	// make sure output format does not change
+	decoder.FormatNone()
+	decoder.Format(rate, channels, mpg123.ENC_SIGNED_16)
+
+	portaudio.Initialize()
+	defer portaudio.Terminate()
+	out := make([]int16, 8192)
+	stream, err := portaudio.OpenDefaultStream(0, channels, float64(rate), len(out), &out)
+	chk(err)
+	defer stream.Close()
+
+	chk(stream.Start())
+	defer stream.Stop()
+	for PluginControl(){
+		audio := make([]byte, 2*len(out))
+		_, err = decoder.Read(audio)
+		if err == mpg123.EOF {
+			break
+		}
+		chk(err)
+
+		chk(binary.Read(bytes.NewBuffer(audio), binary.LittleEndian, out))
+		chk(stream.Write())
+		select {
+		case <-sig:
+			return
+		default:
+		}
+	}
 }
--- models/plugins/plugins.go
+++ models/plugins/plugins.go
@@ -4,12 +4,24 @@ import (
 	"strings"
 	"fmt"
 	"github.com/naryn/maria/models/tts"
+	"time"
 )
+
+const DEFAULT_PLUGIN = "plugins_robot"
+const CMD_STOP = "STOP"
+const CMD_PAUSE = "PAUSE"
+const CMD_RESET = "RESET"
 
 var Plugins map[string]Need
 
+var PluginPause bool
+var PluginStop bool
+var PluginThread string
+
 func init() {
 	fmt.Println("init plugin")
+	PluginPause = false
+	PluginThread = ""
 	//Plugins = make(map[string]Need)
 }
 
@@ -18,26 +30,74 @@ type Need interface {
 	Active() bool
 	Keyword() []string
 	Run(msg string)
+	Control(msg string) string
+	Say(msg string)
 }
 
-func Say(msg string)  {
+func RunBefore(msg string) {
+
+}
+func RunAfter(msg string) {
+
+}
+
+func threadRunPlugin(plugin Need, msg string) {
+	RunBefore(msg)
+	plugin.Run(msg)
+	RunAfter(msg)
+}
+
+func Say(msg string) {
 	tts.Say(msg)
+}
+
+func PluginControl() bool {
+	for PluginPause {
+		time.Sleep(0.8 * time.Second);
+	}
+
+	return !PluginStop
+}
+
+func SendCmd(cmd string) {
+
+	switch cmd {
+	case CMD_STOP:
+		PluginStop = true
+		PluginPause = false
+	case CMD_PAUSE:
+		PluginPause = true
+	case CMD_RESET:
+		PluginPause = false
+		PluginStop = false
+	}
+	time.Sleep(1.3 * time.Second);
 }
 
 func Each(str string) {
 
 	matching := false
-	for _, plugin := range Plugins {
+	for key, plugin := range Plugins {
 
 		for _, w := range plugin.Keyword() {
-			if (strings.Contains(str, w)) {
+			if strings.Contains(str, w) && plugin.Flag() {
 				/* 查看插件是否是启用状态 */
-				if plugin.Flag() {
-					matching = true
-					if plugin.Active(){
-						go plugin.Run(str)
-					}else{
-						go plugin.Run(str)
+				matching = true
+
+				if PluginPause {
+					SendCmd(plugin.Control(str))
+				} else if plugin.Active() {
+					if key != PluginThread && PluginThread != "" {
+						SendCmd(CMD_STOP)
+						PluginThread = ""
+					}
+					if PluginThread == "" {
+						PluginThread = key
+						SendCmd(CMD_RESET)
+						go threadRunPlugin(plugin, str)
+						return
+					} else {
+						//todo
 					}
 
 				}
@@ -47,8 +107,11 @@ func Each(str string) {
 	}
 
 	if matching == false {
-		go Plugins["plugins_robot"].Run(str)
+		SendCmd(CMD_STOP)
+		PluginThread = DEFAULT_PLUGIN
+		go threadRunPlugin(Plugins[DEFAULT_PLUGIN], str)
 	}
+
 }
 
 func Regist(name string, plugin Need) {
@@ -56,4 +119,10 @@ func Regist(name string, plugin Need) {
 		Plugins = make(map[string]Need)
 	}
 	Plugins[name] = plugin
+}
+
+func chk(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
